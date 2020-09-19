e8f043e538110c1693f4bcf0f10e9f654d47e0ea
monu <monu@daffodilsw.com>
2020-09-19 17:57:28 +0530

    wrong test case

--- function.test.js
+++ function.test.js
@@ -1,7 +1,7 @@
 const functions = require('./function')
 
 test('2 to 2 equal 4',()=>{
-    expect(functions.add(2,2)).toBe(4)
+    expect(functions.add(2,2)).toBe(5)
 })
 test('2 to 2 equal 4',()=>{
     expect(functions.add(2,2)).not.toBe(45)
