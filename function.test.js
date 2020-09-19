dd3c053e9231ddc2a10fdd6aa1320b8265510d5e
monu <monu@daffodilsw.com>
2020-09-19 16:21:31 +0530

    initial commit

---
+++ function.test.js
@@ -0,0 +1,8 @@
+const functions = require('./function')
+
+test('2 to 2 equal 4',()=>{
+    expect(functions.add(2,2)).toBe(4)
+})
+test('2 to 2 equal 4',()=>{
+    expect(functions.add(2,2)).not.toBe(45)
+})
